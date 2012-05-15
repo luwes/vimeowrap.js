5fa351816c14a7510282ec3c49d91f9e57528e23
wesley luyten <luytenwesley@gmail.com>
2012-05-15 12:32:03 +0200

    removed js folder

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ java -jar compiler/compiler.jar \
 	--js=src/utils.js \
 	--js=src/signal.js \
 	--js=src/froogaloop.js \
-	--js_output_file=js/vimeowrap.js \
+	--js_output_file=vimeowrap.js \
 	--output_wrapper "if (typeof vimeowrap === 'undefined') {%output%}" \
 	#--compilation_level ADVANCED_OPTIMIZATIONS \
 	
@@ -14,5 +14,5 @@ java -jar compiler/compiler.jar \
 	--js=src/plugins/carousel/carousel.js \
 	--js=src/plugins/carousel/noclickdelay.js \
 	--js=src/plugins/carousel/Tween.js \
-	--js_output_file=js/vimeowrap.carousel.js \
+	--js_output_file=vimeowrap.carousel.js \
 	
