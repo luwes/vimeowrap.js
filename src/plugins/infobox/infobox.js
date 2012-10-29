522bc000995306248ebb643371df66c1742dd654
Wesley Luyten <luytenwesley@gmail.com>
2012-10-29 20:23:08 +0100

    add date time

--- src/plugins/infobox/infobox.js
+++ src/plugins/infobox/infobox.js
@@ -14,7 +14,7 @@
 			position: 'bottom',
 			width: api.config.width,
 			height: 130,
-			template: '<div class="info"><a href="{{user_url}}"><img class="portrait" src="{{user_portrait_medium}}"/></a><div class="title"><a href="{{url}}" title="{{title}}">{{title|truncate:20}}</a></div><div class="byline">from <a href="{{user_url}}">{{user_name}}</a></div><p class="desc">{{description|truncate:100}}</p></div>',
+			template: '<div class="info"><a href="{{user_url}}"><img class="portrait" src="{{user_portrait_medium}}"/></a><div class="title"><a href="{{url}}" title="{{title}}">{{title|truncate:20}}</a></div><div class="byline">from <a href="{{user_url}}">{{user_name}}</a> <time datetime="{{upload_date}}">{{upload_date|timesince}} ago</time></div><p class="desc">{{description|truncate:100}}</p></div>',
 			style: ''
 		};
 
@@ -30,6 +30,7 @@
 						"#{{id}} .portrait { border:1px solid #E1E2E3; background-color:#fff; float:left; padding:2px; margin:0 10px 13px 0; width:52px; height:52px; }" +
 						"#{{id}} .title { font-size:30px; font-weight:700; }" +
 						"#{{id}} .byline a { color:#000; font-weight:700; }" +
+						"#{{id}} .byline time { color:#9DA2A8; font-size:11px; }" +
 						"#{{id}} .desc { clear:both; color:#71767A; font-size:16px; margin:0; }";
 			style += config.style;
 
@@ -71,7 +72,22 @@
 						str = str.slice(0, len) + "...";
 					}
 					return str;
+				},
+
+				timesince: function(str) {
+
+					var obj = { second: (new Date().getTime() - new Date(str).getTime()) / 1000 };
+					var lbl = ["second", "minute", "hour", "day", "week", "month", "year"];
+					var num = [1, 60, 60, 24, 7, 4.34812, 12];
+					for (var i = 1; i < lbl.length; i++) {
+						obj[lbl[i]] = obj[lbl[i-1]] / num[i];
+						if (obj[lbl[i-1]] < num[i]) {
+							var god = Math.floor(obj[lbl[i-1]]);
+							return god + " " + lbl[i-1] + (god === 1 ? "" : "s");
+						}
+					}
 				}
+
 			});
 		}
 	};
