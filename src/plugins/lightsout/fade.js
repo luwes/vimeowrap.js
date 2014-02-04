4317292ac0475ab40cad036db679b596443a9693
Wesley Luyten <luytenwesley@gmail.com>
2014-02-04 14:38:41 +0100

    fix lightsout plugin

--- src/plugins/lightsout/fade.js
+++ src/plugins/lightsout/fade.js
@@ -1,65 +1,60 @@
-(function(global) {
 
-	var Fade = function(element, time, dark, callback) {
-		
-		this.element = element;
-		this.time = time || 1000;
-		this.dark = dark || 0.8;
-		
-		this.opacity = 0;
-		
-		var _this = this;
-		var interval;
-		
-		var supportOpacity = "opacity" in this.element.style;
-        if (!supportOpacity) this.element.style.zoom = 1;
-        
-        function setOpacity(o) {
-            _this.element.style.opacity = "" + o;
-            _this.element.style.filter = "alpha(opacity=" + Math.round(o*100) + ")";
-			_this.opacity = o;
-        }
-		
-		this.off = function() {
-			if (typeof callback === "function") callback();
-			_this.element.style.display = "block";
-			clearInterval(interval);
-			var t0 = new Date().getTime();
-			var o0 = _this.opacity;
-			interval = setInterval(function() {
-				var dt = (new Date().getTime()-t0)/_this.time;
-				if (dt >=1) {
-					dt = 1;
-					clearInterval(interval);
-				}
-				setOpacity(_this.dark*dt+o0*(1-dt));
-			}, 1000 / 60);
-		};
-		
-		this.on = function() {
-			clearInterval(interval);
-			var t0 = new Date().getTime();
-			var o0 = _this.opacity;
-			interval = setInterval(function() {
-				var dt = (new Date().getTime()-t0)/_this.time;
-				if (dt >=1) {
-					dt = 1;
-					clearInterval(interval);
-					_this.element.style.display = "none";
-				}
-				setOpacity(0*dt+o0*(1-dt));
-			}, 1000 / 60);
-		};
-		
-		this.toggle = function() {
-			if (_this.opacity < 0.5) {
-				_this.off();
-			} else {
-				_this.on();
+var Fade = function(element, time, dark, callback) {
+	
+	this.element = element;
+	this.time = time || 1000;
+	this.dark = dark || 0.8;
+	
+	this.opacity = 0;
+	
+	var _this = this;
+	var interval;
+	
+	var supportOpacity = "opacity" in this.element.style;
+	if (!supportOpacity) this.element.style.zoom = 1;
+    
+	function setOpacity(o) {
+		_this.element.style.opacity = "" + o;
+		_this.element.style.filter = "alpha(opacity=" + Math.round(o*100) + ")";
+		_this.opacity = o;
+	}
+
+	this.off = function() {
+		if (typeof callback === "function") callback();
+		_this.element.style.display = "block";
+		clearInterval(interval);
+		var t0 = new Date().getTime();
+		var o0 = _this.opacity;
+		interval = setInterval(function() {
+			var dt = (new Date().getTime()-t0)/_this.time;
+			if (dt >=1) {
+				dt = 1;
+				clearInterval(interval);
 			}
-		};
+			setOpacity(_this.dark*dt+o0*(1-dt));
+		}, 1000 / 60);
 	};
-
-	global.Fade = Fade;
-
-})(vimeowrap.lightsout);
+	
+	this.on = function() {
+		clearInterval(interval);
+		var t0 = new Date().getTime();
+		var o0 = _this.opacity;
+		interval = setInterval(function() {
+			var dt = (new Date().getTime()-t0)/_this.time;
+			if (dt >=1) {
+				dt = 1;
+				clearInterval(interval);
+				_this.element.style.display = "none";
+			}
+			setOpacity(0*dt+o0*(1-dt));
+		}, 1000 / 60);
+	};
+	
+	this.toggle = function() {
+		if (_this.opacity < 0.5) {
+			_this.off();
+		} else {
+			_this.on();
+		}
+	};
+};
--- src/plugins/lightsout/lightsout.js
+++ src/plugins/lightsout/lightsout.js
@@ -5,89 +5,85 @@
  * Version: 1.0 - (2012/06/08)
  * Version: 1.1 - (2012/11/22)
  */
-
-(function(base) {
 	
-	base.lightsout = function(api, config) {
-		var _this = this;
-		var shade, lights;
-		
-		var options = {
-			backgroundcolor:		'000000',
-			opacity:				0.8,
-			time:					800,
-			onplay:					'off',
-			onpause:				'on',
-			onfinish:				'on',
-			parentid:				null
-		};
-
-		config = base.utils.extend(options, config);
-		this.config = config;
-
-		this.setup = function() {
-			
-			shade = document.createElement('div');
-			shade.className += " lightsout_shade";
-			shade.style.display = "none";
-			shade.style.backgroundColor = "#" + config.backgroundcolor;
-			shade.style.zIndex = 300;
-			shade.style.opacity = 0;
-			shade.style.filter = "alpha(opacity=0)";
-			shade.style.top = 0;
-			shade.style.left = 0;
-			shade.style.bottom = 0;
-			shade.style.right = 0;
-			if (config.parentid) {
-				shade.style.position = "absolute";
-				document.getElementById(config.parentid).style.position = "relative";
-				document.getElementById(config.parentid).appendChild(shade);
-			} else {
-				shade.style.position = "fixed";
-				document.body.appendChild(shade);
-			}
-
-			shade.onclick = turnOn;
-			
-			lights = new base.lightsout.Fade(shade, config.time, config.opacity, sortPlayers);
-			this.on = lights.on;
-			this.off = lights.off;
-			this.toggle = lights.toggle;
-
-			api.events.playerReady.add(function(player) {
-				api.onPlay(onPlay);
-				api.onPause(onPause);
-				api.onFinish(onFinish);
-			});
-		};
-
-		function turnOn() {
-			api.pause();
-			lights.on();
-		}
-
-		function onPlay() {
-			if (config.onplay == 'off') lights.off();
-			else lights.on();
-		}
-
-		function onPause() {
-			if (config.onpause == 'off') lights.off();
-			else lights.on();
-		}
-
-		function onFinish() {
-			if (config.onfinish == 'off') lights.off();
-			else lights.on();
-		}
-
-		function sortPlayers() {
-			var players = vimeowrap.getPlayers();
-			for (var key in players) {
-				players[key].display.style.zIndex = "auto";
-			}
-			api.display.style.zIndex = 301;
-		}
+vimeowrap.lightsout = function(api, config) {
+	var _this = this;
+	var shade, lights;
+	
+	var options = {
+		backgroundcolor:		'000000',
+		opacity:				0.8,
+		time:					800,
+		onplay:					'off',
+		onpause:				'on',
+		onfinish:				'on',
+		parentid:				null
 	};
-	
-})(vimeowrap);
+
+	config = vimeowrap.utils.extend(options, config);
+	this.config = config;
+
+	this.setup = function() {
+		
+		shade = document.createElement('div');
+		shade.className += " lightsout_shade";
+		shade.style.display = "none";
+		shade.style.backgroundColor = "#" + config.backgroundcolor;
+		shade.style.zIndex = 300;
+		shade.style.opacity = 0;
+		shade.style.filter = "alpha(opacity=0)";
+		shade.style.top = 0;
+		shade.style.left = 0;
+		shade.style.bottom = 0;
+		shade.style.right = 0;
+		if (config.parentid) {
+			shade.style.position = "absolute";
+			document.getElementById(config.parentid).style.position = "relative";
+			document.getElementById(config.parentid).appendChild(shade);
+		} else {
+			shade.style.position = "fixed";
+			document.body.appendChild(shade);
+		}
+
+		shade.onclick = turnOn;
+		
+		lights = new Fade(shade, config.time, config.opacity, sortPlayers);
+		this.on = lights.on;
+		this.off = lights.off;
+		this.toggle = lights.toggle;
+
+		api.events.playerReady.add(function(player) {
+			api.onPlay(onPlay);
+			api.onPause(onPause);
+			api.onFinish(onFinish);
+		});
+	};
+
+	function turnOn() {
+		api.pause();
+		lights.on();
+	}
+
+	function onPlay() {
+		if (config.onplay == 'off') lights.off();
+		else lights.on();
+	}
+
+	function onPause() {
+		if (config.onpause == 'off') lights.off();
+		else lights.on();
+	}
+
+	function onFinish() {
+		if (config.onfinish == 'off') lights.off();
+		else lights.on();
+	}
+
+	function sortPlayers() {
+		var players = vimeowrap.getPlayers();
+		for (var key in players) {
+			players[key].display.style.zIndex = "auto";
+		}
+		api.display.style.zIndex = 301;
+	}
+};
