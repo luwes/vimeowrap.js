b1d5b06c4cf90847aa59995d26d7ba3103cfe15c
Wesley Luyten <luytenwesley@gmail.com>
2014-05-19 11:47:37 +0200

    fix bug

--- src/plugins/lightsout/fade.js
+++ src/plugins/lightsout/fade.js
@@ -14,19 +14,23 @@ var Fade = function(el, time, dark, callback) {
 	if (!supportOpacity) this.el.style.zoom = 1;
     
 	function setOpacity(o) {
-		_this.el.style.opacity = "" + o;
+		_this.el.style.opacity = '' + o;
 		_this.el.style.filter = 'alpha(opacity=' + Math.round(o*100) + ')';
 		_this.opacity = o;
 
 		//force repaint, Chrome bug
 		_this.el.style.display = 'none';
 		repaint = _this.el.offsetHeight;
-		_this.el.style.display = "";
+		_this.el.style.display = '';
+
+		if (o === 0) {
+			_this.el.style.display = 'none';
+		}
 	}
 
 	this.off = function() {
 		if (typeof callback === 'function') callback();
-		_this.el.style.display = 'block';
+		_this.el.style.display = '';
 		clearInterval(interval);
 		var t0 = +new Date();
 		var o0 = _this.opacity;
@@ -49,7 +53,6 @@ var Fade = function(el, time, dark, callback) {
 			if (dt >=1) {
 				dt = 1;
 				clearInterval(interval);
-				_this.el.style.display = 'none';
 			}
 			setOpacity(0*dt+o0*(1-dt));
 		}, 1000 / 60);
